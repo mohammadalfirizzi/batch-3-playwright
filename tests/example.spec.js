421167f234f925153b55724d8ce9419ce2d5e09f
mohammadalfirizzi <mohammadalfirizzi@gmail.com>
2025-02-01 09:41:26 +0700

    change-testcase-name again

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('testcase pertama nih', async ({ page }) => {
+test('testcase pertama nih lah', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
